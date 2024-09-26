79e7b2bc16934d3d9093c06ce9bbfb33284c8da7
kamilahudda <112380165+kamilahudda@users.noreply.github.com>
2024-09-26 13:07:14 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,16 @@
+//traditionally function
+
+function add2Number(Num1, Num2){
+    let result = num1 + num2
+    return result
+}
+
+//Arrow function
+
+let divide2Number = (num1, num2) => {
+    let result = num1 / num2
+    result return
+}
+
+console.log(add2Number(10,50));
+console.log(divide2Number(50,10));
---
+++ hello.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+
+var firstname = "Nur";
+var lastname = "Kamila";
+
+//My name is Nur Kamila
+console.log("My Name is" + firstname + " " + lastname);
+
+//Strting literal
+console.log(`My name is${firstname} ${lastname}`)
+
+var num1 = 50
+var num2 = 100
+
+console.log(`Multiply Result is ${num1 * num2}`);
+
+let num3 = "10"
+var num1 = 100
+let age;
+
+console.log(typeof num3);
+console.log(typeofage);
